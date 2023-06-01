51f458e91008df72ec2836c4ad04dd9ecbe36ad8
Ignacio CL <icastillejos@gmail.com>
2023-05-31 21:48:16 -0700

    Getting data from request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {ReactComponent as Logo} from './logo.svg'
 import './App.css';
+import Posts from './components/Posts'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Logo style={{height: 50}}/>
         <h1>Hello Radixweb!</h1>
       </header>
+      <Posts />
     </div>
   );
 }
