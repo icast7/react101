ae931529005245d42d134d30e0cc3533bc95f34b
Ignacio CL <icastillejos@gmail.com>
2023-05-31 21:36:33 -0700

    Logo as component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+import {ReactComponent as Logo} from './logo.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo style={{height: 50}}/>
         <h1>Hello Radixweb!</h1>
-
       </header>
     </div>
   );
